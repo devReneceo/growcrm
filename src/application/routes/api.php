d66bf1f73f38817adf44cdccff50200129dda02c
devReneceo <dev@reneceo.com>
2022-08-24 14:43:22 -0700

    production route removed slash

--- src/application/routes/api.php
+++ src/application/routes/api.php
@@ -22,30 +22,27 @@ Route::get('test', [ProgramController::class, 'joel']);
 
 Route::group(['prefix' => 'app'], function () {
     Route::post('daily-report', [ProgramController::class, 'dailyReport']);
-    Route::post('/new-program', [ProgramController::class, 'newProgram']);
-    Route::post('/new-dailyReport', [ProgramController::class, 'newDayReport']);
-    Route::post('/update-dailyReport', [
+    Route::post('new-program', [ProgramController::class, 'newProgram']);
+    Route::post('new-dailyReport', [ProgramController::class, 'newDayReport']);
+    Route::post('update-dailyReport', [
         ProgramController::class,
         'updateDailyReport',
     ]);
-    Route::post('/update-dailyReport-notes', [
+    Route::post('update-dailyReport-notes', [
         ProgramController::class,
         'updateDailyReportNotes',
     ]);
-    Route::post('/add-remainder', [ProgramController::class, 'addRemainder']);
-    Route::post('/dailySettings', [ProgramController::class, 'dailySettings']);
-    Route::post('/get-program-days', [
+    Route::post('add-remainder', [ProgramController::class, 'addRemainder']);
+    Route::post('dailySettings', [ProgramController::class, 'dailySettings']);
+    Route::post('get-program-days', [
         ProgramController::class,
         'getProgramDays',
     ]);
-    Route::post('/isDayCompleted', [
-        ProgramController::class,
-        'isDayCompleted',
-    ]);
+    Route::post('isDayCompleted', [ProgramController::class, 'isDayCompleted']);
 
-    Route::post('/isautoCompleted', [
+    Route::post('isautoCompleted', [
         ProgramController::class,
         'isautoCompleted',
     ]);
-    Route::post('/updatePhotoProgress', [Images::class, 'updatePhotoProgress']);
+    Route::post('updatePhotoProgress', [Images::class, 'updatePhotoProgress']);
 });
