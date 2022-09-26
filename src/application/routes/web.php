7cddc5e3f0111deddd1966adae635a3ef3e5ef29
devReneceo <dev@reneceo.com>
2022-09-25 23:51:58 -0700

    router test2

--- src/application/routes/web.php
+++ src/application/routes/web.php
@@ -1,4 +1,31 @@
 <?php
+
+Route::group(['prefix' => 'app'], function () {
+    Route::post('new-program', [ProgramController::class, 'newProgram']);
+    Route::post('new-dailyReport', [ProgramController::class, 'newDayReport']);
+    Route::post('update-dailyReport', [
+        ProgramController::class,
+        'updateDailyReport',
+    ]);
+    Route::post('update-dailyReport-notes', [
+        ProgramController::class,
+        'updateDailyReportNotes',
+    ]);
+    Route::post('add-remainder', [ProgramController::class, 'addRemainder']);
+    Route::post('dailySettings', [ProgramController::class, 'dailySettings']);
+    Route::post('get-program-days', [
+        ProgramController::class,
+        'getProgramDays',
+    ]);
+    Route::post('isDayCompleted', [ProgramController::class, 'isDayCompleted']);
+
+    Route::post('isautoCompleted', [
+        ProgramController::class,
+        'isautoCompleted',
+    ]);
+    Route::post('updatePhotoProgress', [Images::class, 'updatePhotoProgress']);
+});
+
 //TESTING [DEV]
 Route::any('api-login', 'Authenticate@apilogInAction');
 // custome sig up
